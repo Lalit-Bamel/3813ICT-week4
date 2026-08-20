fa535a46df130e01b2b83d01df383aec68f23cbd
Lalit Bamel <lalitbamel237@gmail.com>
2026-08-20 11:42:39 +1000

    Added home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,51 +1,37 @@
 import { Routes } from '@angular/router';
-
 import { Login } from './login/login';
-
 import { Profile } from './profile/profile';
-
+import { Home } from './home/home';
 import { authGuard } from './auth-guard';
-
 
 export const routes: Routes = [
 
   {
-
     path: 'login',
-
     component: Login
-
   },
 
-
   {
-
-    path: 'profile',
-
-    component: Profile,
-
+    path: 'home',
+    component: Home,
     canActivate: [authGuard]
-
   },
 
+  {
+    path: 'profile',
+    component: Profile,
+    canActivate: [authGuard]
+  },
 
   {
-
     path: '',
-
     redirectTo: 'login',
-
     pathMatch: 'full'
-
   },
 
-
   {
-
     path: '**',
-
     redirectTo: 'login'
-
   }
 
 ];
